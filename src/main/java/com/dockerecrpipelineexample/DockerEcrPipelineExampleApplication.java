586f0f63b016830eac2bcc230667eca634c7ebca
Kanhav Mathur <kanhavmathur05@gmail.com>
2021-07-16 17:35:56 +0530

    Initial Commit

---
+++ src/main/java/com/dockerecrpipelineexample/DockerEcrPipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dockerecrpipelineexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEcrPipelineExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerEcrPipelineExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dockerecrpipelineexample/DockerEcrPipelineExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerecrpipelineexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEcrPipelineExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
